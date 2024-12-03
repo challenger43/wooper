f73674ece25954fff6e1d546a983735d53b89256
Isabella <isabella.lo28@student.tamdistrict.org>
2024-12-03 14:48:05 -0800

    added basic links between pages, started java, made wooper.html

--- index.js
+++ index.js
@@ -4,6 +4,8 @@
 //      Gets money from this(which needs to be linked back to 
 
 
+    
+
 // Money - Khai
 //     Needs to be able to retain information(if possible)
 //     Gets money from doing various things(like food)
@@ -11,12 +13,25 @@
 //     Makes money with Wooper clicker (basically petting Wooper)
 
 
+
+
+
+
+
 // Food treats, drinks etc(eating mechanics) -Emilia 
 //      Wooper food(pop out menu, need to interact with money)
 //       Feeding Wooper increases money 
+
+
+
+
+
+
 
 // Wooper general activity -Isabella 
 //      Wooper needs to be able to walk around, interact when clicked, walk around the screen properly(i.e not going through walls), 
 //      interact with other items(sleep in bed, splash in pool). 
 //      Need random generated actions for Wooper(using functions, like walk forward) 
+let food = 10
+
 
