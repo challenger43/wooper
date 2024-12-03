029c68e46422382b9791ae462bbf9dad27993e33
Isabella <isabella.lo28@student.tamdistrict.org>
2024-12-03 14:20:58 -0800

    javascript stuff assigned
    Co-authored-by: EmiSuzu23 <EmiSuzu23@users.noreply.github.com>
    Co-authored-by: ShadowDev53 <ShadowDev53@users.noreply.github.com>

---
+++ index.js
@@ -0,0 +1,22 @@
+// Petting Wooper-Sydney CODE HERE
+//      Petting Wooper changes Wooper's face expression
+//      Hearts come out depending on how ferociously you pet it. 
+//      Gets money from this(which needs to be linked back to 
+
+
+// Money - Khai
+//     Needs to be able to retain information(if possible)
+//     Gets money from doing various things(like food)
+//     Can spend money on things such as stores etc
+//     Makes money with Wooper clicker (basically petting Wooper)
+
+
+// Food treats, drinks etc(eating mechanics) -Emilia 
+//      Wooper food(pop out menu, need to interact with money)
+//       Feeding Wooper increases money 
+
+// Wooper general activity -Isabella 
+//      Wooper needs to be able to walk around, interact when clicked, walk around the screen properly(i.e not going through walls), 
+//      interact with other items(sleep in bed, splash in pool). 
+//      Need random generated actions for Wooper(using functions, like walk forward) 
+
